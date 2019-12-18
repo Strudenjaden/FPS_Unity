2c88b77ebf17df1ac36d56e0a12f63941c70a06e
Dejan Polit Andres <politdejan8@gmail.com>
2019-12-18 09:23:17 +0100

    Jugador Muere
    Se añade que e jugador muera y se "acabe" el juego

--- Jugador FPS Unity/Assets/Scripts/Player/PlayerHealth.cs
+++ Jugador FPS Unity/Assets/Scripts/Player/PlayerHealth.cs
@@ -12,12 +12,20 @@ public class PlayerHealth : MonoBehaviour
      */
 {
     public float maxLife = 100f; //Maxima vida del jugador.
-
+    public GameObject player;
     public float currentLife; //Vida actual del jugador.
 
     void Start()
     {
         currentLife = maxLife; //Indicamos que al iniciar el juego el jugador obtenga la máxima vida.
+    }
+
+    void Update()
+    {
+        if (currentLife <= 0)
+        {
+            Destroy(player);
+        }
     }
 
 
